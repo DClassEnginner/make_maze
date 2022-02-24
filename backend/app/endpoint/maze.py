b78ef405b30c2c7b8fdfae854aa0afb0d850cc6b
dClassEngineer <66309030+DClassEnginner@users.noreply.github.com>
2022-02-24 21:29:55 +0900

    use pytest

--- backend/app/endpoint/maze.py
+++ backend/app/endpoint/maze.py
@@ -6,6 +6,6 @@ router = APIRouter()
 
 @router.get("/", response_model=schemas.Maze)
 async def get_maze(width: int = 10, height: int = 10, type: MazeType = MazeType.bar):
-    if width < 5 and height < 5:
+    if width < 5 or height < 5:
         raise HTTPException(status_code=400, detail="The width and height of the maze must be at least 5.")
     return make_maze(width, height, type)
--- backend/app/test/test_main.py
+++ backend/app/test/test_main.py
@@ -1,15 +1,44 @@
 from fastapi.testclient import TestClient
-
-from ..main import app
+from app.main import app
 
 client = TestClient(app)
 
-def test_read_maze():
-    response = client.get("/maze/", headers={"X-Token": "coneofsilence"})
+def test_read_maze_bar():
+    response = client.get("/maze/?width=5&height=5&type=bar")
     assert response.status_code == 200
+    maze = response.json()["maze"]
+    assert len(maze) == 5
+    for mz in maze:
+        assert len(mz) == 5
+        for m in mz:
+            assert type(m) == int
+            assert m >= 0 and m <= 1
+    
+    response = client.get("/maze/?width=10&height=10&type=bar")
+    assert response.status_code == 200
+    maze = response.json()["maze"]
+    assert len(maze) == 11
+    for mz in maze:
+        assert len(mz) == 11
+        for m in mz:
+            assert type(m) == int
+            assert m >= 0 and m <= 1
+    
+    response = client.get("/maze/?width=4&height=4&type=bar")
+    assert response.status_code == 400
     assert response.json() == {
-        "id": "foo",
-        "title": "Foo",
-        "description": "There goes my hero",
+        "detail": "The width and height of the maze must be at least 5.",
+    }
+    
+    response = client.get("/maze/?width=5&height=4&type=bar")
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "The width and height of the maze must be at least 5.",
+    }
+    
+    response = client.get("/maze/?width=4&height=5&type=bar")
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "The width and height of the maze must be at least 5.",
     }
 
